800035aed0126fc6c5d02c7d5aa441a87eb46c5f
Łukasz Maciąg <manix93@gmail.com>
2018-07-04 19:12:52 +0200

    Set DiscountRate in view

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -22,7 +23,7 @@ namespace Vidly.Controllers
         // GET: Customers
         public ActionResult Index()
         { 
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
