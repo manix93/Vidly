b3cbf32173dcf5bac70a46488d7bebe052ae9950
Łukasz Maciąg <manix93@gmail.com>
2018-07-04 17:41:17 +0200

    Initial DB

---
+++ Vidly/Models/VidlyDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace Vidly.Models
+{
+    public class VidlyDBContext:DbContext
+    {
+        public VidlyDBContext() {}
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
