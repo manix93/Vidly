95a6e15763c4d3e26aa88bbcaecf8e80407f8eb0
Łukasz Maciąg <manix93@gmail.com>
2018-07-06 14:58:32 +0200

    Add editing Movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -34,15 +34,46 @@ namespace Vidly.Controllers
                 Genres = _context.Genres.ToList()
             };
 
-            return View("MovieForm", movieFormViewModel);
+            return View("MovieCreateForm", movieFormViewModel);
         }
 
-        public ActionResult Save(Movie movie)
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var movieFormViewModel = new MovieFormViewModel()
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieEditForm", movieFormViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
         {
             movie.DateAddedToDatabase = DateTime.Now;
             _context.Movies.Add(movie);
 
             _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+        }
+
+        [HttpPost]
+        public ActionResult Modify(Movie movie)
+        {
+            var _movie = _context.Movies.Single(m => m.Id == movie.Id);
+            _movie.Name = movie.Name;
+            _movie.GenreId = movie.GenreId;
+            _movie.NumberInStock = movie.NumberInStock;
+            _movie.ReleaseDate = movie.ReleaseDate;
+
+            _context.SaveChanges();
+
             return RedirectToAction("Index", "Movies");
         }
 
